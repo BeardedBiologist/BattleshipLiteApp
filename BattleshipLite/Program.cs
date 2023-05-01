934cf447aef47a154b0fd63a5e40009ec7522910
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-01 14:33:25 +0200

    created grid models

---
+++ BattleshipLite/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleshipLiteLibrary.Models;
+
+namespace BattleshipLite
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("created by Joshua James");
+            Console.WriteLine();
+        }
+
+        private static PlayerInfoModel CreatePlayer()
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            // Ask user for their name
+            output.UsersName = AskForUsersName();
+            // Load up the shot grid
+            output.ShotGrid = 
+            // Ask the user for their 5 ship placements 
+            // Clear
+
+        }
+
+        private static string AskForUsersName()
+        {
+            Console.Write("What is your name: ");
+            string output = Console.ReadLine();
+
+            return output;
+        }
+    }
+}
